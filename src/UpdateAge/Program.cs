7f127f121f11054dcf215791cb83224e627ba039
mor39a <89531894+mor39a@users.noreply.github.com>
2025-06-26 19:04:37 -0500

    GitHub Actions added
    I wanted to try GitHub Actions so I made one that would automatically update the age.

---
+++ src/UpdateAge/Program.cs
@@ -0,0 +1,15 @@
+﻿string path = "../../README.md";
+string[] fullFile;
+int age = DateTime.Now.Year - 2007;
+
+if (File.Exists(path))
+{
+    fullFile = File.ReadAllLines(path);
+    fullFile = fullFile.Select(line => line.Trim().StartsWith("public int age =") ? $"{line.Substring(0, line.IndexOf('=') + 1)} {age};" : line).ToArray();
+    File.WriteAllLines(path, fullFile);
+    Console.WriteLine($"Age updated to {age}");
+}
+else
+{
+    Console.WriteLine("Warning! File Not Found");
+}
